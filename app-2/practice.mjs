ac2d3e69457701669d9e1d60433a0e2eed04d797
Tomas Fisher <dontomassino777@gmail.com>
2022-01-05 15:17:52 -0700

    app 2 complete

--- app-2/practice.mjs
+++ app-2/practice.mjs
@@ -1,5 +1,11 @@
 //import module
+import http from "http";
 
 // Save the result of the createServer() method in a variable named server.
-
+const server = http.createServer((req, res) => {
+    res.end('Hello World!');
+});
 // Call the server's listen() method with the port number 3001.
+server.listen(3001, () => {
+    console.log("Cruisin' on 3001");
+});
