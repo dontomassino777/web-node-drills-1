44108448be9f0fe066f49da8306b6292773875c7
Tomas Fisher <dontomassino777@gmail.com>
2022-01-05 14:37:31 -0700

    part 1 complete

---
+++ true-or-false.js
@@ -0,0 +1,8 @@
+function add(a,b) {
+return (a+b)
+}
+add(4,9)
+let array = [[],[{age: 21, color: "blue"}]]
+if(array[0]===[]){ console.log("It's truthy!")}else{console.log("False")}
+if(array[0]==[]){ console.log("It's truthy!")}else{console.log("False")}
+if(array[0]=[]){ console.log("It's truthy!")}else{console.log("False")}
